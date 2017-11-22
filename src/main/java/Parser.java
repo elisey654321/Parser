d13f8189e665a478a308dd449c259c0c2aa52c0c
rigvix <rigvix@gmail.com>
2017-11-22 11:01:01 +0300

    CSS селекторы перенесены в константы

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -8,9 +8,19 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
-
-
 public class Parser {
+
+    private static final String CSS_DATE_SELECTOR = "time[class=time forecast-briefly__date]";
+
+    private static final String CSS_DAY_TEMPERATURE_SELECTOR = "div[class=temp forecast-briefly__temp forecast-briefly__temp_day]";
+
+    private static final String CSS_NIGHT_TEMPERATURE_SELECTOR = "div[class=temp forecast-briefly__temp forecast-briefly__temp_night]";
+
+    private static final String CSS_BRIEFLY_CONDITION_SELECTOR = "div[class=forecast-briefly__condition]";
+
+    private static final String CSS_TODAY_WEATHER_BLOCK_SELECTOR = "div[class=forecast-briefly__day forecast-briefly__day_weekstart_0 day-anchor i-bem]";
+
+    private static final String CSS_DAYS_WEATHER_BLOCKS_SELECTOR = "div[class=forecast-briefly__day day-anchor i-bem]";
 
     private static Document getPage () throws IOException {
         String url = "https://yandex.ru/pogoda/salavat";
@@ -21,10 +31,10 @@ public class Parser {
     private static ArrayList<String[]> data = new ArrayList();
 
     private static String[] printInfoWht (Element str) {
-        String answerDate = str.select ("time[class=time forecast-briefly__date]").text ();
-        String answerTempDay = str.select ("div[class=temp forecast-briefly__temp forecast-briefly__temp_day]").text ();
-        String answerTempNight = str.select ("div[class=temp forecast-briefly__temp forecast-briefly__temp_night]").text ();
-        String answerWth = str.select ("div[class=forecast-briefly__condition]").text ();
+        String answerDate = str.select (CSS_DATE_SELECTOR).text ();
+        String answerTempDay = str.select (CSS_DAY_TEMPERATURE_SELECTOR).text ();
+        String answerTempNight = str.select (CSS_NIGHT_TEMPERATURE_SELECTOR).text ();
+        String answerWth = str.select (CSS_BRIEFLY_CONDITION_SELECTOR).text ();
         return new String[]{answerDate, answerTempDay, answerTempNight, answerWth};
     }
 
@@ -39,9 +49,9 @@ public class Parser {
                 "Состояние погоды"
         };
         Document page= getPage();
-        //css query language
-        Element tableWthFirst = page.select("div[class=forecast-briefly__day forecast-briefly__day_weekstart_0 day-anchor i-bem]").first ();
-        Elements tableWthRest = page.select("div[class=forecast-briefly__day day-anchor i-bem]");
+
+        Element tableWthFirst = page.select(CSS_TODAY_WEATHER_BLOCK_SELECTOR).first ();
+        Elements tableWthRest = page.select(CSS_DAYS_WEATHER_BLOCKS_SELECTOR);
         String[] first = printInfoWht(tableWthFirst);
 
         data.add (first) ;
